89d16c3bef839ea9e9eaba4388d4d52c71fe1b9e
Mokgaetji <florinah.langa@gmail.com>
2026-06-16 14:43:33 +0200

    updated BrowserFactory

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -32,6 +32,7 @@ public class BrowserFactory {
         else {
             driver = new InternetExplorerDriver();
         }
+        //driver.manage().window().maximize();
         driver.manage().window().setSize(new Dimension(1920,1080));
         driver.get(url);
         return driver;
