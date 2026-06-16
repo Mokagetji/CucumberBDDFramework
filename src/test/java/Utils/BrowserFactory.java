d1db4d6efa0e6a3abbfe0c33c276af354c5da28a
Mokgaetji <florinah.langa@gmail.com>
2026-06-16 15:17:17 +0200

    updated browser factory

--- src/test/java/Utils/BrowserFactory.java
+++ src/test/java/Utils/BrowserFactory.java
@@ -14,23 +14,26 @@ public class BrowserFactory {
     public static WebDriver driver;
 
     public static WebDriver startBrowser(String browserChoice, String url) {
-        EdgeOptions edgeOptions = new EdgeOptions();
-
 
         if (browserChoice.equalsIgnoreCase("edge")) {
+            EdgeOptions edgeOptions = new EdgeOptions();
             edgeOptions.addArguments("--headless"); //the pipeline should always run in a headless mode
             edgeOptions.addArguments("--window-size=1920,1080");
+            edgeOptions.addArguments("--disable-gpu");
+
             driver = new EdgeDriver(edgeOptions);
         }
         else if (browserChoice.equalsIgnoreCase("chrome")) {
-            driver = new ChromeDriver();
-        }
-        else if (browserChoice.equalsIgnoreCase("safari")) {
-            driver = new SafariDriver();
+            ChromeOptions chromeOptions = new ChromeOptions();
+            chromeOptions.addArguments("--headless"); //the pipeline should always run in a headless mode
+            chromeOptions.addArguments("--window-size=1920,1080");
+            chromeOptions.addArguments("--disable-gpu");
+
+            driver = new ChromeDriver(chromeOptions);
         }
 
         else {
-            driver = new InternetExplorerDriver();
+            throw new RuntimeException("Unsupported browser: " + browserChoice);
         }
         //driver.manage().window().maximize();
         driver.manage().window().setSize(new Dimension(1920,1080));
